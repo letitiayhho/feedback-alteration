dd50a53c51f3d2fda17579d62baeafacf818b5f6
Letitia Ho <letitiayhho@gmail.com>
2021-06-03 10:33:19 -0400

    Clean up script

--- parse_data.py
+++ parse_data.py
@@ -2,13 +2,9 @@ from pathlib import Path
 import csv
 import math
 
-def normal_round(n):
-    if n - math.floor(n) < 0.5:
-        return math.floor(n)
-    return math.ceil(n)
 
 def parse_pitch_tier(path):
-    print(path.stem) #remove
+    print("parsing:", path.stem) #remove
     values = []
     start_offset = None
     index = 0
@@ -31,42 +27,19 @@ def parse_pitch_tier(path):
                 if start_offset is None:
                     start_offset = number
 
-                expected_number = start_offset + (index * 0.001)
-
-                while abs(number - expected_number) < 1e-5:
+                while True:
+                    expected_number = start_offset + (index * 0.001)
+                    if abs(number - expected_number) < 1e-5:
+                        break
                     values.append(float("NaN"))
                     index += 1
                     size += 1
-                    expected_number = start_offset + (index * 0.001)
-
-                """
-                # Get the recorded time stamp
-                _, number = line.split("=")
-                print(number)
-                number = normal_round(float(number.strip())*1000)
-                print("rounded:", number)
-
-                # Correct for start offset
-                if start_offset is None:
-                    start_offset = number
-                number = number - start_offset
-
-                # If there is a missing value append nan
-                while number != index:
-                    if number < index:
-                        print("index:", index, "number:", number, "point:", point, "value: nan")
-                        raise ValueError(f"Index {index}  exceedes number {number}")
-                    values.append(float("nan"))
-                    index += 1
-                    size += 1
-                """
 
             # Record pitch value
             if "value" in line:
                 _, value = line.split("=")
                 value = float(value.strip())
                 values.append(value)
-                #print("index:", index, "number:", number, "point:", point, "value:", value)
                 index += 1
 
     # Checks
