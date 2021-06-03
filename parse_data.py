e284462fcc17f2a6505cbfb826bb297e1a682a7a
Letitia Ho <letitiayhho@gmail.com>
2021-06-03 10:30:11 -0400

    Kevin added stuff, used an epsilon ball instead of trying to round to the exact right number

--- parse_data.py
+++ parse_data.py
@@ -1,19 +1,50 @@
 from pathlib import Path
 import csv
+import math
 
+def normal_round(n):
+    if n - math.floor(n) < 0.5:
+        return math.floor(n)
+    return math.ceil(n)
 
 def parse_pitch_tier(path):
+    print(path.stem) #remove
     values = []
     start_offset = None
     index = 0
 
     with open(path) as f:
         for line in f:
+            if "size" in line:
+                _, size = line.split("=")
+                size = float(size.strip())
+
+            if "points " in line:
+                _, point = line.split("[")
+                point = float(point[:-3])
+
             if "number" in line:
 
+                _, number = line.split("=")
+                number = float(number.strip())
+
+                if start_offset is None:
+                    start_offset = number
+
+                expected_number = start_offset + (index * 0.001)
+
+                while abs(number - expected_number) < 1e-5:
+                    values.append(float("NaN"))
+                    index += 1
+                    size += 1
+                    expected_number = start_offset + (index * 0.001)
+
+                """
                 # Get the recorded time stamp
                 _, number = line.split("=")
-                number = round(float(number.strip())*1000)
+                print(number)
+                number = normal_round(float(number.strip())*1000)
+                print("rounded:", number)
 
                 # Correct for start offset
                 if start_offset is None:
@@ -22,24 +53,29 @@ def parse_pitch_tier(path):
 
                 # If there is a missing value append nan
                 while number != index:
+                    if number < index:
+                        print("index:", index, "number:", number, "point:", point, "value: nan")
+                        raise ValueError(f"Index {index}  exceedes number {number}")
                     values.append(float("nan"))
-                    print("value: nan") #remove
                     index += 1
-                    print("index:", index) #remove
+                    size += 1
+                """
 
             # Record pitch value
             if "value" in line:
                 _, value = line.split("=")
                 value = float(value.strip())
                 values.append(value)
-                print("value:", value) #remove
+                #print("index:", index, "number:", number, "point:", point, "value:", value)
                 index += 1
-                print("index:", index) #remove
+
+    # Checks
+    if len(values) != size:
+        raise ValueError(f"Values array size {len(values)} doesn't match expected size {size}")
     return values
 
 def parse_file_name(path):
     file_name = path.stem
-    print(file_name)
     (
         subject,
         exp,
@@ -89,64 +125,60 @@ def parse_file_name(path):
 
 DATA_ROOT = Path("./share/hcnlab/IND_all/Exp1_data/")
 
-numbers = parse_pitch_tier("./share/hcnlab/IND_all/Exp1_data/SUBJECT30_20170302_11_[3]/wave_files/pitchtiers/S30_FIH_P1_R1_T1_F3776_U1_HECK_O_BSUBJECT30_20170302_11_[3].PitchTier")
-
-#with open("data.csv", "w") as csvfile:
-#    writer = csv.writer(csvfile)
-#    writer.writerow(
-#        [
-#            "subject",
-#            "exp",
-#            "phasename",
-#            "num_rep",
-#            "num_trial",
-#            "num_formed",
-#            "num_uttered",
-#            "prompt",
-#            "in_or_out",
-#            "date_run",
-#            "time_run",
-#            "block",
-#            "numbers",
-#            "values",
-#        ]
-#    )
-#    for subject_root in DATA_ROOT.iterdir():
-#        if not subject_root.name.startswith("SUBJECT"):
-#            continue
-#        for pitch_tier in (subject_root / "wave_files/pitchtiers").iterdir():
-#            if not pitch_tier.name.endswith(".PitchTier"):
-#                continue
-#            numbers, values = parse_pitch_tier(pitch_tier)
-#            (
-#                subject,
-#                exp,
-#                phasename,
-#                num_rep,
-#                num_trial,
-#                num_formed,
-#                num_uttered,
-#                prompt,
-#                in_or_out,
-#                date_run,
-#                time_run,
-#                block,
-#            ) = parse_file_name(pitch_tier)
-#            writer.writerow(
-#                [
-#                    subject,
-#                    exp,
-#                    phasename,
-#                    num_rep,
-#                    num_trial,
-#                    num_formed,
-#                    num_uttered,
-#                    prompt,
-#                    in_or_out,
-#                    date_run,
-#                    time_run,
-#                    block,
-#                    numbers,
-#                    values,
-#                ]
-#            )
+with open("data.csv", "w") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(
+        [
+            "subject",
+            "exp",
+            "phasename",
+            "num_rep",
+            "num_trial",
+            "num_formed",
+            "num_uttered",
+            "prompt",
+            "in_or_out",
+            "date_run",
+            "time_run",
+            "block",
+            "values",
+        ]
+    )
+    for subject_root in DATA_ROOT.iterdir():
+        if not subject_root.name.startswith("SUBJECT"):
+            continue
+        for pitch_tier in (subject_root / "wave_files/pitchtiers").iterdir():
+            if not pitch_tier.name.endswith(".PitchTier"):
+                continue
+            values = parse_pitch_tier(pitch_tier)
+            (
+                subject,
+                exp,
+                phasename,
+                num_rep,
+                num_trial,
+                num_formed,
+                num_uttered,
+                prompt,
+                in_or_out,
+                date_run,
+                time_run,
+                block,
+            ) = parse_file_name(pitch_tier)
+            writer.writerow(
+                [
+                    subject,
+                    exp,
+                    phasename,
+                    num_rep,
+                    num_trial,
+                    num_formed,
+                    num_uttered,
+                    prompt,
+                    in_or_out,
+                    date_run,
+                    time_run,
+                    block,
+                    values,
+                ]
+            )
